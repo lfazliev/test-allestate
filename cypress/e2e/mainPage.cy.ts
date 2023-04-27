55ee5229546155ca9ac685dca34ae3f35bd41307
Lenar <listok195@gmail.com>
2023-04-28 01:53:57 +0300

    continue main page(sometimes button dsn`t click)

--- cypress/e2e/mainPage.cy.ts
+++ cypress/e2e/mainPage.cy.ts
@@ -3,37 +3,54 @@ describe('mainPage', () => {
         cy.visit('/krasnodar')
         cy.intercept('https://mc.yandex.md/**', { log: false })
         cy.intercept('https://mc.yandex.ru/**', { log: false })
+        cy.get('div.mortgageSection').as('mortgageSection')
+        cy.get('div.mortgageSection').find('a').each(($el, i: number) => {
+            cy.get('@mortgageSection').find('a').eq(i).then((el) => {
+                const href = el.attr('href')
+                if (!href.includes('https://')) {
+                    cy.wrap(el).click()
+                    cy.url().should('include', href)
+                    cy.go('back')
+                }
+            })
 
-        cy.get("div.mortgageSection").contains('жилого дома').click()
-        cy.url().should('include', '/mortgage/programs/secondary')
-        cy.go('back')
-        cy.get("div.mortgageSection").contains('Под залог').click()
-        cy.url().should('include', '/mortgage/programs/secondary_free')
-        cy.go('back')
+        })
 
-        cy.get("div.mortgageSection").contains('от застройщика').click()
-        cy.url().should('include', '/mortgage/programs/new')
-        cy.go('back')
+        cy.get('div.popularBlock').as('popularBlock')
+        cy.get('div.popularBlock').find('a').each(($el, i: number) => {
+            cy.get('@popularBlock').find('a').eq(i).then((el) => {
+                const href = el.attr('href')
+                if (!href.includes('https://')) {
+                    cy.wrap(el).click()
+                    cy.url().should('include', href)
+                    cy.go('back')
+                }
+            })
+        })
+        cy.get('div.newsSection').as('newsSection')
+        cy.get('div.newsSection').find('a').each(($el, i: number) => {
+            cy.get('@newsSection').find('a').eq(i).then((el) => {
+                const href = el.attr('href')
+                if (!href.includes('https://')) {
+                    if (el.is(':visible')) {
+                        cy.wrap(el).click('center')
+                        cy.url().should('include', href)
+                    }
+                    cy.go('back')
+                }
+            })
 
+        })
         cy.get("div.findRieltor").find('button.mainButton').click()
-        cy.get('div.v-overlay__content').find('div.popupModal').find('input[placeholder="Продать"]').type('1')
-        cy.get('div.v-overlay__content').find('div.popupModal').find('input[placeholder="Продать"]').should('have.value', '1')
-
-        cy.get('div.v-overlay__content').find('div.popupModal').find('input[placeholder="Квартира во вторичке"]').type('1')
-        cy.get('div.v-overlay__content').find('div.popupModal').find('input[placeholder="Квартира во вторичке"]').should('have.value', '1')
-
-        cy.get('div.v-overlay__content').find('div.popupModal').find('input[placeholder="Имя"]').type('1')
-        cy.get('div.v-overlay__content').find('div.popupModal').find('input[placeholder="Имя"]').should('have.value', '1')
-
-        cy.get('div.v-overlay__content').find('div.popupModal').find('input[placeholder="+7"]').type('1')
-        cy.get('div.v-overlay__content').find('div.popupModal').find('input[placeholder="+7"]').should('have.value', '1')
-
-        cy.get('div.v-overlay__content').find('div.popupModal').find('textarea[placeholder="Нужен риелтор, специализирующийся на сложных продажах"]').type('1')
-        cy.get('div.v-overlay__content').find('div.popupModal').find('textarea[placeholder="Нужен риелтор, специализирующийся на сложных продажах"]').should('have.value', '1')
-
-        cy.get('div.v-overlay__content').find('div.popupModal').contains('В каком городе?').siblings('div').find('input').invoke('val', '1')
-        cy.get('div.v-overlay__content').find('div.popupModal').contains('В каком городе?').siblings('div').find('input').should('have.value', '1')
-
+        cy.get('div.v-overlay__content').find('div.popupModal').contains('В каком городе?').siblings('div').find('input').invoke('val', '')
+        cy.get('div.v-overlay__content').find('div.popupModal').find('input').each((el, i: number) => {
+            cy.wrap(el).type('1')
+            cy.wrap(el).should('have.value', '1')
+        })
+        cy.get('div.v-overlay__content').find('div.popupModal').find('textarea').then((el) => {
+            cy.wrap(el).type('1')
+            cy.wrap(el).should('have.value', '1')
+        })
         cy.get('div.v-overlay__content').find('div.popupModal').find('a[href="/privacy"]').click()
         cy.url().should('include', '/privacy')
         cy.go('back')
